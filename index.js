dd9c12da9c555e2ca3474ecf1a56a49940be4ab1
Alicia <32777002+akb286@users.noreply.github.com>
2024-04-13 07:25:39 -0600

    update progress bars
    update progress bars

--- index.js
+++ index.js
@@ -14,4 +14,16 @@
       $(this).removeClass("hover");
     }
   );
-  
+
+  // PROGRESS BARS
+const progressBars = document.querySelectorAll(".progress");
+const progressValues = [22, 50, 75, 10, 22, 50, 75, 10]; // Set different progress values for each bar
+
+const changeProgress = (progressBars, progressValues) => {
+  progressBars.forEach((progressbar, index) => {
+    progressbar.style.width = `${progressValues[index]}%`;
+  });
+};
+
+/* change progress after 1 second (only for showcase) */
+setTimeout(() => changeProgress(progressBars, progressValues), 1000);
